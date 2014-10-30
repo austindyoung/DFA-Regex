58bc93e195e78012cf54538f09e9980c9384010f
Austin Young <youngaustin.d@gmail.com>
2014-10-30 02:44:01 -0700

    -a

--- src/main/scala/Regex.scala
+++ src/main/scala/Regex.scala
@@ -1,14 +1,28 @@
 package com.austinyoung.dfaregex
 
-import scala.collection.immutable.HashSet
+import scala.collection._
+import scala.collection.immutable
 
-abstract class Regex[T]
-case class Word[T](content: Seq[T]) extends Regex[T]
-case class Lang[T](language: HashSet[Seq[T]]) extends Regex[T]
-case class *[T](regex: Regex[T]) extends Regex[T] 
-case class U[T](regex1: Regex[T], regex2: Regex[T]) extends Regex[T]
-case class o[T](regex1: Regex[T], regex2: Regex[T]) extends Regex[T]
-
+abstract class Regex[T] {
+  def toDFA: DFA[T]
+}
+case class Word[T](content: Seq[T]) extends Regex[T] {
+  def toDFA = {
+  val op = (letter: T, stateList: List[NFAState[T]]) => (new TransitionMapNFAState[T](Map(letter -> stateList.head), false)) :: stateList
+  val states = content.foldRight(List(finalState))(op)
+  lazy val finalState: NFAState[T] = new TransitionMapNFAState(Map(),true)
+  (new NFA[T](states.head, states, content.toSet.map((letter: T) => (NonEmpty(letter))))).DFA
+  }
+}
+case class *[T](regex: Regex[T]) extends Regex[T] {
+  def toDFA = regex.toDFA.*
+}
+case class U[T](regex1: Regex[T], regex2: Regex[T]) extends Regex[T] {
+  def toDFA = regex1.toDFA.union(regex2.toDFA)
+}
+case class +[T](regex1: Regex[T], regex2: Regex[T]) extends Regex[T] {
+  def toDFA = regex1.toDFA.+(regex2.toDFA)
+}
 object REPLDFA {
   def justChar(character: Char) = {
     lazy val start = new TransitionMapNFAState[Char](
